8a7c07886f1e92cdc4c26a861bce89b0bb9758fe
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-06-09 23:21:50 -0300

    refactor: :zap: improved Payment Confirmation

--- src/schemas/payment-schemas.ts
+++ src/schemas/payment-schemas.ts
@@ -1,26 +1,9 @@
 import { CreatePaymentParams } from '@/repositories/payment-repository';
 import Joi from 'joi';
 
-const expireDateValidationSchema = Joi.string().length(7).custom(joiExpireDateValidation).required();
-
 export const createPaymentSchema = Joi.object<CreatePaymentParams>({
   enrollmentId: Joi.number().required(),
   ticketModality: Joi.string().required(),
   ticketAccomodation: Joi.boolean().required(),
-  name: Joi.string().min(3).required(),
-  cardNumber: Joi.string().length(16).required(),
-  expireDate: expireDateValidationSchema,
-  cvc: Joi.string().length(3).required(),
+  ticketValue: Joi.string().required(),
 });
-
-function joiExpireDateValidation(value: string, helpers: Joi.CustomHelpers<string>) {
-  const today = new Date();
-
-  if (!value) return value;
-
-  if (parseInt(value.split('/')[0]) >= 12 && parseInt(value.split('/')[1]) <= today.getFullYear()) {
-    return helpers.error('any.invalid');
-  }
-
-  return value;
-}
