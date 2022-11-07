56457c4626d106a0043790fc2c4d6de73e0917f1
lesimoes <lesimoes0@gmail.com>
2022-11-07 11:11:10 -0300

    removed comment

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,15 +1,9 @@
 import authenticationService, { SignInParams } from "@/services/authentication-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
-//For eslint boundaries check only
-//import sessionRepository from "@/repositories/session-repository";
 
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
-  // sessionRepository.create({
-  //   userId: 0,
-  //   token: ""
-  // });
 
   try {
     const result = await authenticationService.signIn({ email, password });
